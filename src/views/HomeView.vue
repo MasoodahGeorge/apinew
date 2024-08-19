1f09c83cd33dac4ff4e499c49b8f6cd52c1a4b2e
MasoodahGeorge <oodasbusiness@gmail.com>
2024-08-19 09:19:01 +0200

    commit msg

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src0
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+
+
---
+++ src/views/loginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'loginView',
+//   components: {
+//     loginView
+//   }
+// }
+</script>
+
+<style scoped>
+
+</style>
